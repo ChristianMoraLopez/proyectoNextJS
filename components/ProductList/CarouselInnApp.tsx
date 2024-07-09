5cebe45d1cba49ec21d87e8b4b2266a75dccdddb
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-07-09 08:01:31 -0500

    images added

--- components/ProductList/CarouselInnApp.tsx
+++ components/ProductList/CarouselInnApp.tsx
@@ -9,23 +9,23 @@ export default function CarouselImages() {
     <Carousel className= 'mb-8'style={{ height: '600px' }}>
       <img
         alt="..."
-        src="https://media.discordapp.net/attachments/941971306004504638/1135690965932318862/MichelleGreen_In_an_art_gallery_various_paintings_and_unique_wo_d562693c-07fe-4c70-a000-33c00b6076f4.png?width=676&height=676"
+        src="/images/Designer.jpeg"
       />
       <img
         alt="..."
-        src="https://media.discordapp.net/attachments/941971306004504638/1135690943903834112/MichelleGreen_In_an_art_gallery_various_paintings_and_unique_wo_aab04689-38e3-4f48-8c34-ad7d6f131127.png?width=676&height=676"
+        src="/images/Designer1.jpeg"
       />
       <img
         alt="..."
-        src="https://media.discordapp.net/attachments/945077390839787570/1135480627743961098/meebee_large_carved_block_of_melted_colorful_plastic_on_a_tabl_b777636b-96e1-4bbd-9ed5-0a0c981a0221.png?width=794&height=676"
+        src="/images/Designer2.jpeg"
       />
       <img
         alt="..."
-        src="https://media.discordapp.net/attachments/989739710081531985/1134167946684551288/jeanguelre_a_cinematic_front_shot_of_a_cool_indoor_wide_open_ar_18ad8165-7b94-4ef8-9a22-7ed70a99a20b.png?width=1206&height=676"
+        src="/images/Designer3.jpeg"
       />
       <img
         alt="..."
-        src="https://media.discordapp.net/attachments/1072899954525356072/1129249856519684167/storhuvud_real_life_birddragondinosaur_colorfull_pink_full_siz_35ef0f86-1352-45c6-9165-176546070603.png?width=676&height=676"
+        src="/images/Designer4.jpeg"
       />
     </Carousel>
 <section className= 'mb-14'>
